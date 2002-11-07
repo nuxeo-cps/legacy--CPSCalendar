5595594f93b17cdc1dc0d395f5cc8c7095587477
jalon <devnull@localhost>
2002-11-07 07:37:49 +0000

    - add member attendees (using portal_metadirectories) - delete actions, asking for confirmations according to events statuses - some more minor view fixes

---
+++ skins/calendar_delevent.py
@@ -0,0 +1,29 @@
+##parameters=comment=None, notify=None, REQUEST=None
+
+event = context
+
+if comment is not None:
+    calendar = event.getCalendar()
+    if notify == 'decline':
+        event.setMyStatus('decline', comment)
+    elif notify == 'cancel':
+        event.setEventStatus('canceled')
+        event.updateAttendeesCalendars(comment=comment)
+    calendar.manage_delObjects([event.id])
+else:
+    noconfirm_cancel = (not event.attendees) or \
+        (not event.canEditThisEvent()) or \
+        (event.event_status == 'canceled' and not event.isdirty) or \
+        (not [att for att in event.attendees if att['status'] != 'decline'])
+    confirm_decline = (not event.canEditThisEvent()) and \
+        event.getMyStatus() != 'decline' and \
+        event.event_status != 'canceled'
+    if confirm_decline:
+        return event.calendar_confirmdelevent(notify='decline')
+    if not noconfirm_cancel:
+        return event.calendar_confirmdelevent(notify='cancel')
+    calendar = event.getCalendar()
+    calendar.manage_delObjects([event.id])
+
+if REQUEST is not None:
+    REQUEST.RESPONSE.redirect(calendar.absolute_url())
--- skins/calendar_getDispInformations.py
+++ skins/calendar_getDispInformations.py
@@ -18,16 +18,6 @@ elif disp == 'day':
 elif disp == 'month':
     current_action = 'month_view'
 
-# determine current selected day
-selected_day = REQUEST.form.get('selected_day')
-if selected_day is not None:
-    selected_day = DateTime(int(selected_day))
-    REQUEST.SESSION['calendar_selected'] = selected_day
-else:
-    selected_day = REQUEST.SESSION.get('calendar_selected')
-    if selected_day is None:
-        selected_day = DateTime()
-
 # determine current viewed day
 viewed_day = REQUEST.form.get('viewed_day')
 if viewed_day is not None:
@@ -37,6 +27,13 @@ else:
     viewed_day = REQUEST.SESSION.get('calendar_viewed')
     if viewed_day is None:
         viewed_day = DateTime()
+
+# determine current selected day
+selected_day = REQUEST.form.get('selected_day')
+if selected_day is not None:
+    selected_day = DateTime(int(selected_day))
+else:
+    selected_day = viewed_day
 
 time_since_daystart = viewed_day.hour()*3600+viewed_day.minute()*60+viewed_day.second()
 timeTime = viewed_day.timeTime()
