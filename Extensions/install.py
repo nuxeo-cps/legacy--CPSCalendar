9c738be76ca0502e9e0c4980226dbd0ac1493cc7
edegoute <devnull@localhost>
2003-06-23 13:08:53 +0000

    fix deletion of default domain

--- Extensions/install.py
+++ Extensions/install.py
@@ -82,126 +82,9 @@ def cpscalendarinstall(self):
         portal.portal_skins.addSkinSelection(skin_name, npath)
         pr(" Fixup of skin %s" % skin_name)
         
-##    # create workflow
 ##    pr("Setup workflow shemas")
     wftool = portal.portal_workflow
     trtool = portal.portal_trees
-##    wfids = wftool.objectIds()
-
-##    # WF calendars
-##    wfid = 'calendar_folder_wf'
-##    pr(" Setup workflow %s" % wfid)
-##    if wfid in wfids:
-##        wftool.manage_delObjects([wfid])
-##    wftool.manage_addWorkflow(id=wfid,
-##                              workflow_type='cps_workflow (Web-configurable workflow for CPS)')
-##    wf = wftool[wfid]
-    
-##    for s in ('work', ):
-##        wf.states.addState(s)
-##    for t in ('create', 'create_content', 'create_folder'):
-##        wf.transitions.addTransition(t)
-
-##    s = wf.states.get('work')
-##    s.setProperties(title='Work', 
-##                    transitions=('create_content',))
-##    t = wf.transitions.get('create')
-##    t.setProperties(title='Initial creation', new_state_id='work', 
-##                    transition_behavior=(TRANSITION_INITIAL_CREATE, ), 
-##                    clone_allowed_transitions=None,
-##                    actbox_name='', actbox_category='workflow', actbox_url='',
-##                    props={'guard_permissions':'', 
-##                           'guard_roles':'Manager; WorkspaceManager; WorkspaceMember', 
-##                           'guard_expr':''},
-##                    )
-##    t = wf.transitions.get('create_content')
-##    t.setProperties(title='Create content', new_state_id='work', 
-##                    transition_behavior=(TRANSITION_ALLOWSUB_CREATE, ), 
-##                    clone_allowed_transitions=None,
-##                    trigger_type=TRIGGER_USER_ACTION, 
-##                    actbox_name='New',
-##                    actbox_category='',
-##                    actbox_url='',
-##                    props={'guard_permissions':'', 
-##                           'guard_roles':'Manager; WorkspaceManager; WorkspaceMember', 
-##                           'guard_expr':''},
-##                    )
-
-##    # WF workspace content
-##    wfid = 'calendar_content_wf'
-##    pr(" Setup workflow %s" % wfid)
-##    if wfid in wfids:
-##        wftool.manage_delObjects([wfid])
-##    wftool.manage_addWorkflow(id=wfid,
-##                              workflow_type='cps_workflow (Web-configurable workflow for CPS)')
-##    wf = wftool[wfid]
-    
-##    for s in ('work', ):
-##        wf.states.addState(s)
-##    for t in ('create', 'copy_submit', ):
-##        wf.transitions.addTransition(t)
-##    for v in ('action', 'actor', 'comments', 'review_history', 'time'):
-##        wf.variables.addVariable(v)
-##    for p in (View, ModifyPortalContent, ):
-##        wf.addManagedPermission(p)
-        
-##    s = wf.states.get('work')
-##    s.setProperties(title='Work', 
-##                    transitions=('copy_submit',))
-##    s.setPermission(ModifyPortalContent, 0, ('Manager', 'WorkspaceManager', 'WorkspaceMember'))
-##    s.setPermission(View, 0, ('Manager', 'WorkspaceManager', 'WorkspaceMember', 'WorkspaceReader'))
-
-##    t = wf.transitions.get('create')
-##    t.setProperties(title='Initial creation', new_state_id='work', 
-##                    transition_behavior=(TRANSITION_INITIAL_CREATE, ),
-##                    clone_allowed_transitions=None,
-##                    actbox_name='', actbox_category='workflow', actbox_url='',
-##                    props={'guard_permissions':'',
-##                           'guard_roles':'Manager; WorkspaceManager; WorkspaceMember', 
-##                           'guard_expr':''},
-##                    )
-##    t = wf.transitions.get('copy_submit')
-##    t.setProperties(title='Copy content into a section for Publishing',
-##                    new_state_id='', 
-##                    transition_behavior=(TRANSITION_BEHAVIOR_PUBLISHING, ), 
-##                    clone_allowed_transitions=('submit', 'publish'),
-##                    trigger_type=TRIGGER_USER_ACTION, 
-##                    actbox_name='Submit', actbox_category='workflow',
-##                    actbox_url='%(content_url)s/content_submit_form',
-##                    props={'guard_permissions':'', 
-##                           'guard_roles':'Manager; WorkspaceManager; WorkspaceMember', 
-##                           'guard_expr':''},
-##                    )
-    
-##    # wf variables
-##    wf.variables.setStateVar('review_state')
-##    vdef = wf.variables['action']
-##    vdef.setProperties(description='The last transition',
-##                       default_expr='transition/getId|nothing',
-##                       for_status=1, update_always=1)
-
-##    vdef = wf.variables['actor']
-##    vdef.setProperties(description='The ID of the user who performed '
-##                       'the last transition',
-##                       default_expr='user/getId',
-##                       for_status=1, update_always=1)
-
-##    vdef = wf.variables['comments']
-##    vdef.setProperties(description='Comments about the last transition',
-##                       default_expr="python:state_change.kwargs.get('comment', '')",
-##                       for_status=1, update_always=1)
-
-##    vdef = wf.variables['review_history']
-##    vdef.setProperties(description='Provides access to workflow history',
-##                       default_expr="state_change/getHistory",
-##                       props={'guard_permissions':'',
-##                              'guard_roles':'Manager; WorkspaceManager; WorkspaceMember; WorkspaceReader',
-##                              'guard_expr':''})
-
-##    vdef = wf.variables['time']
-##    vdef.setProperties(description='Time of the last transition',
-##                       default_expr="state_change/getDateTime",
-##                       for_status=1, update_always=1)
 
 
     # setup portal_type: CPS Proxy Document, CPS Proxy Folder
@@ -318,9 +201,6 @@ def cpscalendarinstall(self):
     if calendars_id not in portal[workspace_id].objectIds():
         portal[workspace_id].portal_workflow.invokeFactoryFor(portal[workspace_id].this(), 'Calendars',
                                                 calendars_id)
-        #if 1:
-        #    pr(dir(portal[workspace_id].members))
-        #    return 'stop'
         portal[workspace_id].members.getContent().setTitle('Root of members calendars') # XXX L10N
         portal[workspace_id].members.reindexObject()
         pr("  Adding %s Folder" % calendars_id)
@@ -341,18 +221,6 @@ def cpscalendarinstall(self):
         portal[workspace_id].members.manage_permission(perm, roles, 0)
         pr("  Permission %s" % perm)
     portal[workspace_id].members.reindexObjectSecurity()
-    
-##    pr("Verifying local workflow association")
-##    if not '.cps_workflow_configuration' in portal[calendars_id].objectIds():
-##        pr("  Adding workflow configuration to %s" % calendars_id)
-##        portal[calendars_id].manage_addProduct['CPSCore'].addCPSWorkflowConfiguration()
-##        wfc = getattr(portal[calendars_id], '.cps_workflow_configuration')
-##        wfc.manage_addChain(portal_type='Calendar',
-##                            chain='workspace_folder_wf')
-##        wfc.manage_addChain(portal_type='Calendars',
-##                            chain='workspace_folder_wf')
-##        wfc.manage_addChain(portal_type='Event',
-##                            chain='workspace_content_wf')
 
     # init Tree Tool
     pr("Verifying cache trees")
@@ -369,17 +237,18 @@ def cpscalendarinstall(self):
 
     # i18n
     pr(" Adding i18n support")
+    calendar_catalog_id='cpscalendar'
 
     Localizer = portal['Localizer']
     # languages
     languages = Localizer.get_supported_languages()
 
     # MessageCatalog
-    if 'cpscalendar' in Localizer.objectIds():
-        Localizer.manage_delObjects(['cpscalendar'])
+    if calendar_catalog_id in Localizer.objectIds():
+        Localizer.manage_delObjects([calendar_catalog_id])
         pr("  previous default MessageCatalog deleted")
     Localizer.manage_addProduct['Localizer'].manage_addMessageCatalog(
-        id='cpscalendar',
+        id=calendar_catalog_id,
         title='CPSCalendar messages',
         languages=languages,
         )
@@ -414,9 +283,11 @@ def cpscalendarinstall(self):
     if portalhas('translation_service'):
         translation_service = portal.translation_service
 
-        # translation domains
-        translation_service.manage_setDomainInfo(path_0='Localizer/cpscalendar')
-        pr("   cpscalendar domain set to Localizer/default")
+        if not portal['translation_service'].cpscalendar:
+            # translation domains
+            translation_service.manage_addDomainInfo(calendar_catalog_id,
+                                                     'Localizer/'+calendar_catalog_id)
+            pr("   cpscalendar domain set to Localizer/cpscalendar")
 
         pr(" Reindexing catalog")
         portal.portal_catalog.refreshCatalog(clear=1)
