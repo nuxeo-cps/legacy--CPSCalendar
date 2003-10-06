9f7971b9efbc6ba374ff3e839158ce50ab3d1cf7
madarche <devnull@localhost>
2003-10-06 15:25:28 +0000

    Moved the normalize method after the initialization of all the attributes

--- Event.py
+++ Event.py
@@ -126,11 +126,11 @@ class Event(CPSBaseDocument):
             self.setAttendees(attendees)
         self.from_date = from_date
         self.to_date = to_date
-        self._normalize()
         self.location = kw.get('location')
         self.event_status = kw.get('event_status')
         self.category = kw.get('category')
         self.transparent = kw.get('transparent')
+        self._normalize()
 
     security.declareProtected('Modify portal content', 'edit')
     def edit(self, attendees=None, from_date=None, to_date=None, 
