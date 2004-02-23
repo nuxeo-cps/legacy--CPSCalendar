90d5b7553c7a5e60947a9911954da3bedd5a8a5a
sfermigier <devnull@localhost>
2004-02-23 13:24:56 +0000

    Cosmetic.

--- skins/cps_calendar/getUserTypes.py
+++ skins/cps_calendar/getUserTypes.py
@@ -5,18 +5,15 @@ return [
         'id': 'member',
         'title': 'cpscalendar_member',
         'plural_title': 'cpscalendar_members',
-    },
-    {
+    }, {
         'id': 'room',
         'title': 'cpscalendar_room',
         'plural_title': 'cpscalendar_rooms',
-    },
-    {
+    }, {
         'id': 'resource',
         'title': 'cpscalendar_resource',
         'plural_title': 'cpscalendar_resources',
-    },
-    {
+    }, {
         'id': 'events_show',
         'title': "cpscalendar_events_show",
         'plural_title': "cpscalendar_events_shows",
