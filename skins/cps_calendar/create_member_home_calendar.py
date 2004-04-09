f68a24bab2e2f73d986c69ad0df5c7bb37faa5c3
edegoute <devnull@localhost>
2004-04-09 16:01:38 +0000

    add script for create a home calendar

---
+++ skins/cps_calendar/create_member_home_calendar.py
@@ -0,0 +1,13 @@
+##parameters=REQUEST=None
+# $ Id: $
+# Just create an calendar in the user's home folder with «calendar» id
+
+portal = context.portal_url.getPortalObject()
+
+portal.portal_cpscalendar.createMemberCalendar()
+
+if REQUEST is not None:
+    REQUEST.RESPONSE.redirect(
+        '%s/calendar' % portal.portal_membership.getHomeUrl())
+
+return
