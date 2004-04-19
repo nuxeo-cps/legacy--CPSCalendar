19ae2f85dee51734112529cafd0fca191291eaa1
lregebro <devnull@localhost>
2004-04-19 08:18:25 +0000

    Fixed bugs in getting the to and from dates when making meetings.

--- skins/cps_calendar/calendar_addevent_form.py
+++ skins/cps_calendar/calendar_addevent_form.py
@@ -1,3 +1,6 @@
-##parameters=
+##parameters=REQUEST
+
+# Clean it up!
+REQUEST.SESSION['meeting'] = None
 
 return context.calendar_editevent_form(create=1)
--- skins/cps_calendar/calendar_getFromToDate.py
+++ skins/cps_calendar/calendar_getFromToDate.py
@@ -56,23 +56,27 @@ elif event and not create:
 else:
     # No form, no event. This is created by clicking on a add event link.
     if REQUEST.has_key('from_date'):
-        from_date = REQUEST['from_date']
+        from_date = DateTime(int(REQUEST['from_date']))
+        from_date_hour = from_date.hour()
+        from_date_minute = from_date.minute()
     elif REQUEST.has_key('selected_day'):
         from_date = DateTime(int(REQUEST['selected_day']))
+        from_date_hour = DateTime().hour()
+        from_date_minute = DateTime().minute() + 30
     else:
-        from_date = DateTime()
+        from_date = DateTime() 
+        from_date_hour = DateTime().hour()
+        from_date_minute = DateTime().minute() + 30
 
-    if same_type(from_date, 'string'):
-        from_date = context.portal_cpscalendar.stringToDateTime(from_date)
-    elif not same_type(from_date, DateTime()):
-        # Should be an int or something else that is convertable
-        from_date = DateTime(from_date)
+    #if same_type(from_date, 'string'):
+    #    from_date = context.portal_cpscalendar.stringToDateTime(from_date)
+    #elif not same_type(from_date, DateTime()):
+    #    # Should be an int or something else that is convertable
+    #    from_date = DateTime(from_date)
 
     from_date_year = from_date.year()
     from_date_month = from_date.month()
     from_date_day = from_date.day()
-    from_date_hour = DateTime().hour()
-    from_date_minute = DateTime().minute()
     from_date_date = DateTime(from_date_year, from_date_month, from_date_day)
 
     to_date_year = from_date_year
@@ -83,10 +87,10 @@ else:
     to_date_date = from_date_date
 
 # Round minutes to nearest quarter
-from_date_minute = (((from_date_minute)+14)/15)*15;
+from_date_minute = ((from_date_minute)/15)*15;
 if from_date_minute ==60:
     from_date_minute =0
-to_date_minute = (((from_date_minute)+14)/15)*15;
+to_date_minute = ((from_date_minute)/15)*15;
 if to_date_minute ==60:
     to_date_minute =0
 
