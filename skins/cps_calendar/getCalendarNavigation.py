9c542f64a02d5bc197254582b31031d44b5a073c
lregebro <devnull@localhost>
2004-08-11 12:31:40 +0000

    Missing file

---
+++ skins/cps_calendar/getCalendarNavigation.py
@@ -0,0 +1,44 @@
+##parameters=finder, root_uid, current_uid=None, REQUEST=None
+# $Id$
+from Products.CPSNavigation.ConfNavigation import ConfNavigation
+from Products.CPSNavigation.ZODBNavigation import ZODBNavigation
+from Products.CPSNavigation.CPSNavigation import CPSNavigation
+from Products.CPSNavigation.LDAPDirectoryNavigation import \
+     LDAPDirectoryNavigation
+from Products.CPSNavigation.CPSDirectoryNavigation import \
+     CPSDirectoryNavigation
+
+if REQUEST and REQUEST.has_key('current_uid'):
+    current_uid = REQUEST.get('current_uid')
+if not current_uid:
+    current_uid = root_uid
+
+once = 0 # Python doesn't have an until statement. :-/
+next_uid = current_uid
+while not (once and doc and doc.meta_type == 'CPS Proxy Folder'):
+    once = 1
+    current_uid = next_uid
+    docurl = context.portal_url.getPortalPath() + '/' + current_uid
+    doc = context.restrictedTraverse(docurl, None)
+    next_uid = '/'.join(next_uid.split('/')[:-1])
+
+# current_uid = context.portal_url.getRelativeContentURL(context)
+nav = CPSNavigation(root_uid=root_uid,
+                    current_uid=current_uid,
+                    context=context,
+                    include_root=0,
+                    no_leaves=0,
+                    # filter_tree_ptypes=('Workspace',),
+                    # filter_listing_ptypes=('Link',),
+                    sort_listing_by='title',
+                    sort_listing_direction='asc',
+                    batch_size=15,
+                    request_form=REQUEST.form,
+                    )
+# XXX try to get another tree and concatenate ?
+
+tree = None
+if not REQUEST.get('search'):
+    tree = nav.getTree()
+listing, listing_info, batch_info = nav.getListing()
+return tree, listing, batch_info, listing_info, finder
